4ae4f6693e2df28640f361c5e4d77863da01a056
Andrey <kedr_club@mail.ru>
2023-06-02 20:21:20 +0300

    refactor: slider.js

--- scripts/slider.js
+++ scripts/slider.js
@@ -60,126 +60,99 @@ const moveRight = () => {
   RIGHTBUTTON.removeEventListener("click", moveRight);
 };
 
+const showCard = (array) => {
+  array.forEach(el => createCard(el));
+};
+
 LEFTBUTTON.addEventListener("click", moveLeft);
 RIGHTBUTTON.addEventListener("click", moveRight);
 
 // ------------------------------
 
-
-
 if (document.documentElement.clientWidth > 1279) {
+  let nextArray = [0, 1, 2, 3, 4, 5, 6, 7, 0];
+  showCard(nextArray);
 
-let showCard = (array) => {
-  for (let i = 0; i < 9; i++) {
-    createCard(array[i]);
-  }
-}
-
-let startArray = [0, 1, 2, 3, 4, 5, 6, 7, 0];
-showCard(startArray);
-
-let nextArray = startArray;
-
-CAROUSEL.addEventListener("animationend", (animationEvent) => {
-  let oldCards = document.querySelectorAll('.services__slider_item');
-  for (let i = 0; i < 9; i++) {
-    oldCards[i].remove();
-  }
-
-  let newArray = [];
-  function createArrayLeft () {
-    const arr = [0, 1, 2, 3, 4, 5, 6, 7];
-    let newArr = arr;
-    newArr.splice(newArr.indexOf(nextArray[0]), 1);
-    newArr.splice(newArr.indexOf(nextArray[1]), 1);
-    newArr.splice(newArr.indexOf(nextArray[2]), 1);
-    let firstEl = newArr[Math.floor(Math.random() * 5)];
-    newArr.splice(newArr.indexOf(firstEl), 1);
-    newArray.push(firstEl);
-    for (let i = 1; i < 8; i++) {
-      if (i > 2 && i < 6) {
-        newArray.push(nextArray[i - 3]);
-      } else {
-        let nextEl = newArr[Math.floor(Math.random() * (newArr.length))];
-        newArr.splice(newArr.indexOf(nextEl), 1);
-        newArray.push(nextEl);
+  CAROUSEL.addEventListener("animationend", (animationEvent) => {
+    let oldCards = document.querySelectorAll('.services__slider_item');
+    oldCards.forEach(el => el.remove());
+    let newArray = [];
+    
+    function createArrayLeft () {
+      let newArr = [0, 1, 2, 3, 4, 5, 6, 7];
+      newArr.splice(newArr.indexOf(nextArray[0]), 1);
+      newArr.splice(newArr.indexOf(nextArray[1]), 1);
+      newArr.splice(newArr.indexOf(nextArray[2]), 1);
+      let firstEl = newArr[Math.floor(Math.random() * 5)];
+      newArr.splice(newArr.indexOf(firstEl), 1);
+      newArray.push(firstEl);
+      for (let i = 1; i < 8; i++) {
+        if (i > 2 && i < 6) {
+          newArray.push(nextArray[i - 3]);
+        } else {
+          let nextEl = newArr[Math.floor(Math.random() * newArr.length)];
+          newArr.splice(newArr.indexOf(nextEl), 1);
+          newArray.push(nextEl);
+        }
       }
+      newArray.push(firstEl);
+      nextArray = newArray;
     }
-    newArray.push(firstEl);
-    nextArray = newArray;
-  }
 
-  function createArrayRight () {
-    const arr = [0, 1, 2, 3, 4, 5, 6, 7];
-    let newArr = arr;
-    newArr.splice(newArr.indexOf(nextArray[6]), 1);
-    newArr.splice(newArr.indexOf(nextArray[7]), 1);
-    newArr.splice(newArr.indexOf(nextArray[8]), 1);
-    let firstEl = newArr[Math.floor(Math.random() * 5)];
-    newArr.splice(newArr.indexOf(firstEl), 1);
-    newArray.push(firstEl);
-    for (let i = 1; i < 8; i++) {
-      if (i > 2 && i < 6) {
-        newArray.push(nextArray[i + 3]);
-      } else {
-        let nextEl = newArr[Math.floor(Math.random() * (newArr.length))];
-        newArr.splice(newArr.indexOf(nextEl), 1);
-        newArray.push(nextEl);
+    function createArrayRight () {
+      let newArr = [0, 1, 2, 3, 4, 5, 6, 7];
+      newArr.splice(newArr.indexOf(nextArray[6]), 1);
+      newArr.splice(newArr.indexOf(nextArray[7]), 1);
+      newArr.splice(newArr.indexOf(nextArray[8]), 1);
+      let firstEl = newArr[Math.floor(Math.random() * 5)];
+      newArr.splice(newArr.indexOf(firstEl), 1);
+      newArray.push(firstEl);
+      for (let i = 1; i < 8; i++) {
+        if (i > 2 && i < 6) {
+          newArray.push(nextArray[i + 3]);
+        } else {
+          let nextEl = newArr[Math.floor(Math.random() * newArr.length)];
+          newArr.splice(newArr.indexOf(nextEl), 1);
+          newArray.push(nextEl);
+        }
       }
+      newArray.push(firstEl);
+      nextArray = newArray;
     }
-    newArray.push(firstEl);
-    nextArray = newArray;
-  }
 
-
-  if (animationEvent.animationName === "move-left") {
-    CAROUSEL.classList.remove("transition-left");
-    createArrayLeft();
+    if (animationEvent.animationName === "move-left") {
+      CAROUSEL.classList.remove("transition-left");
+      createArrayLeft();
+    } else {
+      CAROUSEL.classList.remove("transition-right");
+      createArrayRight();
+    }
     showCard(newArray);
-  } else {
-    CAROUSEL.classList.remove("transition-right");
-    createArrayRight();
-    showCard(newArray);
-  }
 
-
-  LEFTBUTTON.addEventListener("click", moveLeft);
-  RIGHTBUTTON.addEventListener("click", moveRight);
-})
+    LEFTBUTTON.addEventListener("click", moveLeft);
+    RIGHTBUTTON.addEventListener("click", moveRight);
+  })
 }
-
 
 // -------------------------
 
-
 else if (document.documentElement.clientWidth < 768) {
-  let showCard = (array) => {
-    for (let i = 0; i < 3; i++) {
-      createCard(array[i]);
-    }
-  }
-  
-  let startArray = [2, 3, 4];
-  showCard(startArray);
-  
-  let nextArray = startArray;
+  let nextArray = [2, 3, 4];
+  showCard(nextArray);
   
   CAROUSEL.addEventListener("animationend", (animationEvent) => {
     let oldCards = document.querySelectorAll('.services__slider_item');
-    for (let i = 0; i < 3; i++) {
-      oldCards[i].remove();
-    }
-  
+    oldCards.forEach(el => el.remove());  
     let newArray = [];
+
     function createArrayLeft () {
-      const arr = [0, 1, 2, 3, 4, 5, 6, 7];
-      let newArr = arr;
+      let newArr = [0, 1, 2, 3, 4, 5, 6, 7];
       newArr.splice(newArr.indexOf(nextArray[0]), 1);
       for (let i = 0; i < 3; i++) {
         if (i === 1) {
           newArray.push(nextArray[i - 1]);
         } else {
-          let nextEl = newArr[Math.floor(Math.random() * (newArr.length))];
+          let nextEl = newArr[Math.floor(Math.random() * newArr.length)];
           newArr.splice(newArr.indexOf(nextEl), 1);
           newArray.push(nextEl);
         }
@@ -188,14 +161,13 @@ else if (document.documentElement.clientWidth < 768) {
     }
 
     function createArrayRight () {
-      const arr = [0, 1, 2, 3, 4, 5, 6, 7];
-      let newArr = arr;
+      let newArr = [0, 1, 2, 3, 4, 5, 6, 7];
       newArr.splice(newArr.indexOf(nextArray[2]), 1);
       for (let i = 0; i < 3; i++) {
         if (i === 1) {
           newArray.push(nextArray[i + 1]);
         } else {
-          let nextEl = newArr[Math.floor(Math.random() * (newArr.length))];
+          let nextEl = newArr[Math.floor(Math.random() * newArr.length)];
           newArr.splice(newArr.indexOf(nextEl), 1);
           newArray.push(nextEl);
         }
@@ -206,52 +178,37 @@ else if (document.documentElement.clientWidth < 768) {
     if (animationEvent.animationName === "move-left") {
       CAROUSEL.classList.remove("transition-left");
       createArrayLeft();
-      showCard(newArray);
     } else {
       CAROUSEL.classList.remove("transition-right");
       createArrayRight();
-      showCard(newArray);
     }
-  
+    showCard(newArray);
   
     LEFTBUTTON.addEventListener("click", moveLeft);
     RIGHTBUTTON.addEventListener("click", moveRight);
   })
-  
-
 }
 
 // -------------------------
 
 else {
-    let showCard = (array) => {
-    for (let i = 0; i < 6; i++) {
-      createCard(array[i]);
-    }
-  }
-  
-  let startArray = [1, 2, 3, 4, 5, 6];
-  showCard(startArray);
-  
-  let nextArray = startArray;
+  let nextArray = [1, 2, 3, 4, 5, 6];
+  showCard(nextArray);
   
   CAROUSEL.addEventListener("animationend", (animationEvent) => {
     let oldCards = document.querySelectorAll('.services__slider_item');
-    for (let i = 0; i < 6; i++) {
-      oldCards[i].remove();
-    }
-  
+    oldCards.forEach(el => el.remove());  
     let newArray = [];
+
     function createArrayLeft () {
-      const arr = [0, 1, 2, 3, 4, 5, 6, 7];
-      let newArr = arr;
+      let newArr = [0, 1, 2, 3, 4, 5, 6, 7];
       newArr.splice(newArr.indexOf(nextArray[0]), 1);
       newArr.splice(newArr.indexOf(nextArray[1]), 1);
       for (let i = 0; i < 6; i++) {
         if (i > 1 && i < 4) {
           newArray.push(nextArray[i - 2]);
         } else {
-          let nextEl = newArr[Math.floor(Math.random() * (newArr.length))];
+          let nextEl = newArr[Math.floor(Math.random() * newArr.length)];
           newArr.splice(newArr.indexOf(nextEl), 1);
           newArray.push(nextEl);
         }
@@ -260,15 +217,14 @@ else {
     }
 
     function createArrayRight () {
-      const arr = [0, 1, 2, 3, 4, 5, 6, 7];
-      let newArr = arr;
+      let newArr = [0, 1, 2, 3, 4, 5, 6, 7];
       newArr.splice(newArr.indexOf(nextArray[4]), 1);
       newArr.splice(newArr.indexOf(nextArray[5]), 1);
       for (let i = 0; i < 6; i++) {
         if (i > 1 && i < 4) {
           newArray.push(nextArray[i + 2]);
         } else {
-          let nextEl = newArr[Math.floor(Math.random() * (newArr.length))];
+          let nextEl = newArr[Math.floor(Math.random() * newArr.length)];
           newArr.splice(newArr.indexOf(nextEl), 1);
           newArray.push(nextEl);
         }
@@ -279,26 +235,13 @@ else {
     if (animationEvent.animationName === "move-left") {
       CAROUSEL.classList.remove("transition-left");
       createArrayLeft();
-      showCard(newArray);
     } else {
       CAROUSEL.classList.remove("transition-right");
       createArrayRight();
-      showCard(newArray);
     }
-  
+    showCard(newArray);
   
     LEFTBUTTON.addEventListener("click", moveLeft);
     RIGHTBUTTON.addEventListener("click", moveRight);
   })
-  
-}
-
-
-
-
-
-
-
-
-
-
+};
