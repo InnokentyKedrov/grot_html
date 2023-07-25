3232a8108562c49058310b3822a3bb079bb28728
Andrey <kedr_club@mail.ru>
2023-07-25 04:39:34 +0300

    fix: burger close button

--- scripts/burger.js
+++ scripts/burger.js
@@ -1,32 +1,32 @@
-const burger = document.querySelector('.burger-btn');
-const blackout = document.querySelector('.blackout');
-const headerNavigation = document.querySelector('.header__navigation');
-const logo = document.querySelector('.logo');
+const burger = document.querySelector(".burger-btn");
+const blackout = document.querySelector(".blackout");
+const headerNavigation = document.querySelector(".header__navigation");
+const logo = document.querySelector(".logo");
 
 if (burger) {
   burger.addEventListener("click", () => {
-      document.body.classList.add('lock');
-      burger.classList.add('burger-btn_active');
-      blackout.classList.add('blackout_active');
-      headerNavigation.classList.add('header__navigation_active');
-      logo.classList.add('logo_active');
-    });
+    document.body.classList.toggle("lock");
+    burger.classList.toggle("burger-btn_active");
+    blackout.classList.toggle("blackout_active");
+    headerNavigation.classList.toggle("header__navigation_active");
+    logo.classList.toggle("logo_active");
+  });
 }
 
 blackout.addEventListener("click", () => {
-  document.body.classList.remove('lock');
-  burger.classList.remove('burger-btn_active');
-  blackout.classList.remove('blackout_active');
-  headerNavigation.classList.remove('header__navigation_active');
-  logo.classList.remove('logo_active');
+  open();
 });
 
-if (!document.body.classList.contains('lock')) {
+if (!document.body.classList.contains("lock")) {
   headerNavigation.addEventListener("click", () => {
-    document.body.classList.remove('lock');
-    burger.classList.remove('burger-btn_active');
-    blackout.classList.remove('blackout_active');
-    headerNavigation.classList.remove('header__navigation_active');
-    logo.classList.remove('logo_active');
+    open();
   });
-  }
+}
+
+const open = () => {
+  document.body.classList.remove("lock");
+  burger.classList.remove("burger-btn_active");
+  blackout.classList.remove("blackout_active");
+  headerNavigation.classList.remove("header__navigation_active");
+  logo.classList.remove("logo_active");
+};
